b6dba055c003811ff296cbe05414740a87f47a63
Muhammad Saqib Sarwar <saqib@inspirythemes.com>
2016-01-01 17:59:08 +0500

    initial commit

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+/*-----------------------------------------------------------------------------------*/
+/*	Enqueue Styles in Child Theme
+/*-----------------------------------------------------------------------------------*/
+if (!function_exists('inspiry_enqueue_child_styles')) {
+    function inspiry_enqueue_child_styles(){
+        if ( !is_admin() ) {
+            // dequeue and deregister parent default css
+            wp_dequeue_style( 'parent-default' );
+            wp_deregister_style( 'parent-default' );
+
+            // dequeue parent custom css
+            wp_dequeue_style( 'parent-custom' );
+
+            // parent default css
+            wp_enqueue_style( 'parent-default', get_template_directory_uri().'/style.css' );
+
+            // parent custom css
+            wp_enqueue_style( 'parent-custom' );
+
+            // child default css
+            wp_enqueue_style('child-default', get_stylesheet_uri(), array('parent-default'), '1.0', 'all' );
+
+            // child custom css
+            wp_enqueue_style('child-custom',  get_stylesheet_directory_uri() . '/child-custom.css', array('child-default'), '1.0', 'all' );
+        }
+    }
+}
+add_action( 'wp_enqueue_scripts', 'inspiry_enqueue_child_styles', PHP_INT_MAX );
+
+
+if ( !function_exists( 'inspiry_load_translation_from_child' ) ) {
+    /**
+     * Load translation files from child theme
+     */
+    function inspiry_load_translation_from_child() {
+        load_child_theme_textdomain ( 'framework', get_stylesheet_directory () . '/languages' );
+    }
+    add_action ( 'after_setup_theme', 'inspiry_load_translation_from_child' );
+}
