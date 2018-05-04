f5cd0de6cba546b798c89de1f94138abb9dc4908
Muhammad Saqib Sarwar <saqib@inspirythemes.com>
2018-05-04 21:31:58 +0500

    improved custom css and js directory structure

--- functions.php
+++ functions.php
@@ -22,10 +22,10 @@ if ( ! function_exists( 'inspiry_enqueue_child_styles' ) ) {
 			wp_enqueue_style( 'child-default', get_stylesheet_uri(), array( 'parent-default' ), '1.0', 'all' );
 
 			// child custom css
-			wp_enqueue_style( 'child-custom', get_stylesheet_directory_uri() . '/child-custom.css', array( 'child-default' ), '1.0', 'all' );
+			wp_enqueue_style( 'child-custom', get_stylesheet_directory_uri() . '/css/child-custom.css', array( 'child-default' ), '1.4', 'all' );
 
 			// child custom js
-			wp_enqueue_script( 'child-custom', get_stylesheet_directory_uri() . '/child-custom.js', array( 'jquery' ), '1.0', true );
+			wp_enqueue_script( 'child-custom', get_stylesheet_directory_uri() . '/js/child-custom.js', array( 'jquery' ), '1.4', true );
 		}
 	}
 }
