8aa047a9e181fa6fc24b8b812c510efc88522f0f
Fahid <fahid@inspirythemes.com>
2020-11-27 15:53:43 +0500

    Revert "Improved child theme’s functions.php file code."
    This reverts commit 07e575a97d9c40bd48cb0ff2d00a404f6e2fedfc.

--- functions.php
+++ functions.php
@@ -1,60 +1,31 @@
 <?php
-/**
- * RealHomes Child theme functions.
- *
- * @package realhomes-child
- */
-
+/*-----------------------------------------------------------------------------------*/
+/*	Enqueue Styles in Child Theme
+/*-----------------------------------------------------------------------------------*/
 if ( ! function_exists( 'inspiry_enqueue_child_styles' ) ) {
-	/**
-	 * Enqueue Styles in Child Theme.
-	 */
 	function inspiry_enqueue_child_styles() {
 		if ( ! is_admin() ) {
-			// dequeue and deregister parent default css.
+			// dequeue and deregister parent default css
 			wp_dequeue_style( 'parent-default' );
 			wp_deregister_style( 'parent-default' );
 
-			// dequeue parent custom css.
+			// dequeue parent custom css
 			wp_dequeue_style( 'parent-custom' );
 
-			// parent default css.
-			wp_enqueue_style(
-				'parent-default',
-				get_template_directory_uri() . '/style.css',
-				array(),
-				INSPIRY_THEME_VERSION,
-			);
+			// parent default css
+			wp_enqueue_style( 'parent-default', get_template_directory_uri() . '/style.css' );
 
-			// parent custom css.
+			// parent custom css
 			wp_enqueue_style( 'parent-custom' );
 
-			// child default css.
-			wp_enqueue_style(
-				'child-default',
-				get_stylesheet_uri(),
-				array( 'parent-default' ),
-				'1.0',
-				'all'
-			);
+			// child default css
+			wp_enqueue_style( 'child-default', get_stylesheet_uri(), array( 'parent-default' ), '1.0', 'all' );
 
-			// child custom css.
-			wp_enqueue_style(
-				'child-custom',
-				get_stylesheet_directory_uri() . '/css/child-custom.css',
-				array( 'child-default' ),
-				'1.4',
-				'all'
-			);
+			// child custom css
+			wp_enqueue_style( 'child-custom', get_stylesheet_directory_uri() . '/css/child-custom.css', array( 'child-default' ), '1.4', 'all' );
 
-			// child custom js.
-			wp_enqueue_script(
-				'child-custom',
-				get_stylesheet_directory_uri() . '/js/child-custom.js',
-				array( 'jquery' ),
-				'1.4',
-				true
-			);
+			// child custom js
+			wp_enqueue_script( 'child-custom', get_stylesheet_directory_uri() . '/js/child-custom.js', array( 'jquery' ), '1.4', true );
 		}
 	}
 }
@@ -63,7 +34,7 @@ add_action( 'wp_enqueue_scripts', 'inspiry_enqueue_child_styles', PHP_INT_MAX );
 
 if ( ! function_exists( 'inspiry_load_translation_from_child' ) ) {
 	/**
-	 * Load translation files from child theme.
+	 * Load translation files from child theme
 	 */
 	function inspiry_load_translation_from_child() {
 		load_child_theme_textdomain( 'framework', get_stylesheet_directory() . '/languages' );
